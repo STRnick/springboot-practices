e78cf6eff93b39f7239903f635db7cbaf2af7ec1
STRnick <nicksumin97@naver.com>
2022-06-22 10:22:42 +0900

    myapplication #1

--- hellospringboot/src/main/java/ex01/HelloSpringBootApplication.java
+++ hellospringboot/src/main/java/ex01/HelloSpringBootApplication.java
@@ -1,10 +1,8 @@
 package ex01;
 
-import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 
